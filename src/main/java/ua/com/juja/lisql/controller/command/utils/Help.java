a08b68abe7210a536ca21065d718ba6ce59a9ca8
Alex <sumy.ua@gmail.com>
2017-02-24 10:00:23 +0200

    Help

--- src/main/java/ua/com/juja/lisql/controller/command/utils/Help.java
+++ src/main/java/ua/com/juja/lisql/controller/command/utils/Help.java
@@ -1,7 +1,22 @@
 package ua.com.juja.lisql.controller.command.utils;
 
-/**
- *
- */
-public class Help {
+import ua.com.juja.lisql.controller.command.ViewCommand;
+import ua.com.juja.lisql.view.Message;
+import ua.com.juja.lisql.view.View;
+
+public class Help extends ViewCommand {
+
+    public Help(View view) {
+        super(view);
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return command.equalsIgnoreCase("help");
+    }
+
+    @Override
+    public void process(String command) {
+        getView().write(Message.HELP.toString());
+    }
 }
