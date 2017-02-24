f28e63c5c5eafee10bc10e555d4af7b6180d84a8
Alex <sumy.ua@gmail.com>
2017-02-24 12:02:38 +0200

    Close command

--- src/main/java/ua/com/juja/lisql/controller/command/utils/Close.java
+++ src/main/java/ua/com/juja/lisql/controller/command/utils/Close.java
@@ -1,7 +1,28 @@
 package ua.com.juja.lisql.controller.command.utils;
 
-/**
- *
- */
-public class Close {
+import ua.com.juja.lisql.controller.command.ViewCommand;
+import ua.com.juja.lisql.view.View;
+
+public class Close extends ViewCommand {
+
+    private static boolean isCalled;
+
+    public Close(View view) {
+        super(view);
+        isCalled = true;
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return isCalled;
+    }
+
+    @Override
+    public void process(String command) {
+        // Корректно закрыть программу
+    }
+
+    public static boolean isCalled() {
+        return isCalled;
+    }
 }
