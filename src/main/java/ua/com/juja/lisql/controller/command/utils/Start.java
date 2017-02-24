57db472b8a26a219c277a87cb22cb90a77d8f232
Alex <sumy.ua@gmail.com>
2017-02-24 11:26:11 +0200

    Start command

--- src/main/java/ua/com/juja/lisql/controller/command/utils/Start.java
+++ src/main/java/ua/com/juja/lisql/controller/command/utils/Start.java
@@ -1,7 +1,26 @@
 package ua.com.juja.lisql.controller.command.utils;
 
+import ua.com.juja.lisql.controller.command.ViewCommand;
+import ua.com.juja.lisql.view.Message;
+import ua.com.juja.lisql.view.View;
+
 /**
- *
+ * Команда Start запускает заставку-приветствие и приглашение пользователю
+ * Безусловный вызов при старте программы
  */
-public class Start {
+public class Start extends ViewCommand {
+
+    public Start(View view) {
+        super(view);
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return true;
+    }
+
+    @Override
+    public void process(String command) {
+        getView().write(Message.HELLO.toString(), Message.START.toString());
+    }
 }
