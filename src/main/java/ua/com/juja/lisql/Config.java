37f5e0d7b931e30a4775e4055cb6ee4556632d8c
Alex <sumy.ua@gmail.com>
2017-05-16 13:55:26 +0300

    resource bundle - проверил

--- src/main/java/ua/com/juja/lisql/Config.java
+++ src/main/java/ua/com/juja/lisql/Config.java
@@ -2,9 +2,5 @@ package ua.com.juja.lisql;
 
 public interface Config {
     String PROPERTIES = "src/main/resources/";
-    String RESOURCES = "src/main/resources/ua.com.juja.lisql.view.bundle";
-//    String COMMON = "src/main/resources/ua.com.juja.lisql.view.bundle/common.properties";
     String COMMON = "ua.com.juja.lisql.view.bundle.common";
-//    ua.com.vadim.SQLcmd.controller.resources.interface.
-
 }
