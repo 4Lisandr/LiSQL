c92055208d93695f7339c6c031961893767630b1
Alex <sumy.ua@gmail.com>
2019-03-20 12:04:38 +0200

    Controller - lambda refactoring

--- src/main/java/ua/com/juja/lisql/controller/Controller.java
+++ src/main/java/ua/com/juja/lisql/controller/Controller.java
@@ -2,8 +2,8 @@ package ua.com.juja.lisql.controller;
 
 import ua.com.juja.lisql.controller.command.Command;
 import ua.com.juja.lisql.controller.command.connection.Connect;
-import ua.com.juja.lisql.controller.command.read.TablesList;
 import ua.com.juja.lisql.controller.command.read.Find;
+import ua.com.juja.lisql.controller.command.read.TablesList;
 import ua.com.juja.lisql.controller.command.utils.*;
 import ua.com.juja.lisql.controller.command.write.*;
 import ua.com.juja.lisql.model.DatabaseManager;
@@ -11,10 +11,10 @@ import ua.com.juja.lisql.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
-import static ua.com.juja.lisql.controller.command.TextBundle.FAIL;
-import static ua.com.juja.lisql.controller.command.TextBundle.INPUT;
-import static ua.com.juja.lisql.controller.command.TextBundle.RETRY;
+import static ua.com.juja.lisql.controller.command.TextBundle.*;
 
 /**
  * Controller of commands, singleton
@@ -73,11 +73,9 @@ public final class Controller {
 
         private static void handle(String input) {
             try {
-                for (Command command : getAll()) {
-                    if (command.runIfReady(input)) {
-                        break;
-                    }
-                }
+                getAll().stream()
+                        .filter(c -> c.runIfReady(input))
+                        .findFirst();
             } catch (Exception e) {
                 printError(e);
             }
@@ -85,11 +83,9 @@ public final class Controller {
 
         public static List<Command> getAll() {
             ArrayList<Command> result = new ArrayList<>();
-            for (UsersCommand usr : values()) {
-                if (usr != null) {
-                    result.add(usr.getCommand());
-                }
-            }
+            Stream.of(values())
+                    .filter(Objects::nonNull)
+                    .forEach(usr -> result.add(usr.getCommand()));
             return result;
         }
 
